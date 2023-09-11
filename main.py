cc06a180465e0b40dcade4e5a6fa2f5bee2f80db
Radinmirzaee <rdnmirzaee@gmail.com>
2023-09-11 18:25:23 +0330

    questions and options were added

--- main.py
+++ main.py
@@ -7,4 +7,18 @@ def display_score():
 def play_again():
     pass
 
+questions = {"How many elements are in the periodic table?: ": "C",
+             "Which animal lays the largest eggs?: ": "D",
+             "What is the most abundant gas in Earth's atmosphere?: ": "A",
+             "How many bones are in the human body?: ": "A",
+             "Which planet in the solar system is the hottest?: ": "B"
+             }
+
+options = [["A. 116", "B. 117", "C. 118", "D. 119"],
+           ["A. Whale", "B. Crocodile", "C. Elephant", "D. Ostrich"],
+           ["A. Nitrogen", "B. Oxygen", "C. Carbon-Dioxide", "D. Hydrogen"],
+           ["A. 206", "B. 207", "C. 208", "D. 209"],
+           ["A. Mercury", "B. Venus", "C. Earth", "D. Mars"]]
+
+
 new_game()
